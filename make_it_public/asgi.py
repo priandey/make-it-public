ac34ae8e1f11869fcc0aff42e9b966680e62815e
Pierre Riandey <42967943+priandey@users.noreply.github.com>
2023-02-25 11:43:44 +0100

    create django project

---
+++ make_it_public/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for make_it_public project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'make_it_public.settings')
+
+application = get_asgi_application()
