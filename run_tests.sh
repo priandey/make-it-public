99b764c4fc094b74d65c50fc1bd7b4c9295d9400
Pierre Riandey <42967943+priandey@users.noreply.github.com>
2023-03-11 15:09:03 +0100

    (helpers) add coverage report html generation

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run the Django tests and output coverage report
-docker-compose run web sh -c "coverage run --branch manage.py test -v2 --keepdb && coverage report"
+docker-compose run web sh -c "coverage run --branch manage.py test -v2 --keepdb && coverage report && coverage html --skip-empty"
