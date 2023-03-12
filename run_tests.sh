82e12dd3b785d13fe8da0b80d55faa7a0db2a71c
Archenior <archenior@sd-113319.dedibox.fr>
2023-03-12 10:52:52 +0100

    add aliases support

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+shopt -s expand_aliases
+source ~/.profile
 # Run the Django tests and output coverage report
 docker-compose run web sh -c "coverage run --branch manage.py test -v2 --keepdb && coverage report && coverage html --skip-empty"
